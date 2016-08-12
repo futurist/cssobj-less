bc2da3e3dcc2145fc83e706324db85df565a08bf
1111hui <26634873@qq.com>
2016-08-12 17:29:11 +0800

    ADD: es6 import

--- src/index.js
+++ src/index.js
@@ -1,25 +1,66 @@
-var extend = require('objutil').extend
-var normalize = require('./normalize.js')
-var scaffolding = require('./scaffolding.js')
-var alert = require('./alert.js')
-var lessHelper = require('./less-helper.js')
+
+import {extend} from 'objutil'
+import * as normalize from './normalize.js'
+import * as scaffolding from './scaffolding.js'
+import * as alert from './alert.js'
+import {lessValuePlugin} from './less-helper.js'
 
 var obj = extend(
   {},
-  normalize,
   scaffolding,
   alert
 )
 
-var result = cssobj(obj, {
-  onUpdate: cssobj_plugin_post_csstext(function(v) {
-    console.log(v)
-  }),
-  plugins:{
-    value: lessHelper.lessValuePlugin()
+// bs extend will overwrite normalize, so seperate it
+cssobj(normalize)
+
+// var result = cssobj(obj, {
+//   onUpdate: cssobj_plugin_post_csstext(function(v) {
+//     console.log(v)
+//   }),
+//   plugins:{
+//     value: lessValuePlugin()
+//   }
+// })
+
+// console.log(result)
+
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      obj: obj,
+      result: cssobj(obj, {local: props.local, onUpdate:cssobj_plugin_post_csstext(function(v) {
+        console.log(v)
+      }), plugins:{
+        value: lessValuePlugin()
+      } })
+    }
+    this.updateCSS = this.updateCSS.bind(this)
   }
-})
 
-console.log(result)
+  updateCSS () {
+    this.state.obj['.nav'].fontSize = '34px'
+    this.state.result.update()
+  }
 
+  render () {
+    var result = this.state.result
+    return (
+      <div className="alert-wrapper">
+        <div className={result.mapClass("alert alert-danger")} role="alert"><strong>Well done!</strong> You successfully read this important alert message. </div>
+        <div className={result.mapClass("alert alert-danger")} role="alert"> <strong>Heads up!</strong> This alert needs your attention, but it's not super important. </div>
+      <div className={result.mapClass("alert alert-danger")} role="alert"> <strong>Warning!</strong> Better check yourself, you're not looking too good. </div>
+        <div className={result.mapClass("alert alert-danger")} role="alert"> <strong>Oh snap!</strong> Change a few things up and try submitting again. </div>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App local={true} />, document.getElementById('container'))
 
