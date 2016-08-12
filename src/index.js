6189d3ab1c2f21b915b5130dbb72e4812f2f9fa0
1111hui <26634873@qq.com>
2016-08-12 22:43:11 +0800

    ADD: parseStr for AST in less expression

--- src/index.js
+++ src/index.js
@@ -83,3 +83,98 @@ window.onload = function() {
     $(v).onchange()
   })
 }
+
+
+var str = 'ceil((@font-size-base * 1.7))'
+
+
+function splitComma (str, comma) {
+  var sep = []
+  for (var c, i = 0, n = 0, prev = 0, d = []; c = str.charAt(i); i++) {
+    if (c == '(' || c == '[') n++
+    if (c == ')' || c == ']') n--
+    if (!n && comma.test(c)) d.push(str.substring(prev, i), c), prev = i + 1
+  }
+  return d.concat(str.substring(prev))
+}
+
+console.log( 3333, parseStr(str) )
+
+function parseStr(val, paramN) {
+  var rs = '', args, prefix=paramN > 0? ',' : ''
+
+  val += ''
+
+
+  // val = ColorNames[val] || val
+
+  // color #333
+  var match = val.match(/^\s*#([0-9A-F]+)(.*)$/i)
+  if(match) return prefix + match[1] + parseStr(match[2]) //new Color(val.slice(1))
+
+  // color rgba()
+  var match = val.match(/^\s*rgba?\(([^)]*)\)(.*)$/i)
+  if(match) {
+    var alpha=1, rgba = match[1].split(',')
+    var rgb = rgba.length>3 ? (alpha=rgba.pop(), rgba) : rgba
+    return prefix + match[1] + parseStr(match[2]) // new Color(rgb, alpha)
+  }
+
+  // dimension 10px
+  var match = val.match(/^([0-9.-]+)([a-z%]*)(.*)$/i)
+  if(match) return prefix + match[1] + (match[2]||'') + parseStr(match[3]) // new Dimension(match[1], match[2])
+
+  // ceil()
+  var match = val.match(/^\s*([a-z]+)\((.*)\s*$/i)
+  if(match) {
+    return prefix + 'getFuncion("'+match[1]+ '"' + parseStr(match[2], 1)
+  }
+
+  // ceil()
+  var match = val.match(/^\s*\((.*)\s*$/i)
+  if(match) {
+    return prefix + 'Operation(' + parseStr(match[1], 0)
+  }
+
+  // +-*/
+  var match = val.match(/^\s*([\+\-\*\/])(.*)\s*$/)
+  if (match) {
+    return prefix + '"-'+match[1]+'-"' + parseStr(match[2])
+  }
+
+  // @var
+  var match = val.match(/^\s*(@[a-z0-9$-]+)(.*)\s*$/i)
+  if(match) {
+    return prefix + 'getVar("'+match[1]+'")' + parseStr(match[2])
+  }
+
+  if(/^[\s)]*$/.test(val)) return val
+
+  // var func = val.match(/^\s*([a-z]+)\((.*)\)(.*)\s*$/i)
+  // if(0&&func) {
+  //   rs += 'getFuncion("'+ m[1] +'"'
+  //   args = splitComma(m[2], /,/)
+  //   for(var i = 0; i < args.length; i+=2) {
+  //     rs += parseStr(args[i]) + (args[i+1]||'')
+  //   }
+  //   rs += ')'
+  // }
+
+  // // (2px + 3px)
+  // var operate = val.match(/^\s*\((.*)\)(.*)\s*$/)
+  // var operateRe = /^\s*[+-*/]\s*/
+  // if(0&&operate) {
+  //   args = splitComma(operate[1], operateRe)
+  //   rs += args.map(function(v) {
+  //     return parseStr(v)
+  //   }).reduce(function(prev, cur) {
+  //     if(prev.length%3 == 0) prev.push('Operation(' + cur)
+  //     if(prev.length%3 == 1) prev.push(cur)
+  //     if(prev.length%3 == 2) prev.push(cur + ')')
+  //   }, []).join('')
+  // }
+
+  return val
+}
+
+
